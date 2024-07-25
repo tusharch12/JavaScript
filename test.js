1028e9879b5d6af81f829726a3b471bb274d3819
Tushar Chaturvedi <tusharch6577@gmail.com>
2024-07-25 17:46:04 +0000

    .

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
+"use strict" // to use newest version of javascript
 console.log("hello, ");
 console.log("Tushar Don")
+
+var acc_num = '1236427437';
+let acc_name = "Tushar";
+
+console.table([acc_num,acc_name]);
+
+//null => Stand alone data type : data typed of null is object
+// undefined => when value is not defined 
